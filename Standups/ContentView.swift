84560f732057dc2dd098c62cb363d2e3807a5090
Siran Li <siranli411@gmail.com>
2023-11-26 14:06:22 -0600

    Initial Commit

---
+++ Standups/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Standups
+//
+//  Created by Siran Li on 11/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Siran Li on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
