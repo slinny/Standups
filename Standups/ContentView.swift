e1e34e93f7d6a3fad9de490cac22e6e656e2247d
Siran Li <siranli411@gmail.com>
2023-12-03 02:52:21 -0600

    Updated StandupsList

--- Standups/ContentView.swift
+++ Standups/ContentView.swift
@@ -1,24 +1,19 @@
-//
-//  ContentView.swift
-//  Standups
-//
-//  Created by Siran Li on 11/26/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
     }
+    .padding()
+  }
 }
 
-#Preview {
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
     ContentView()
+  }
 }
